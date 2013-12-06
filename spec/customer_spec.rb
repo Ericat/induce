b6a3b07cb97744e3eedab1f1486946bef95128f4
Ericat <erica.salvaneschi@hotmail.com>
2013-12-06 18:54:57 +0000

    Edited README and added customer.name

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,13 +1,19 @@
-# require './customer'
+require './customer'
 
-# describe Customer do
+describe Customer do
   
-#   let(:customer) { Customer.new }
-#   let(:takeaway) { double :takeaway }
+  let(:customer) { Customer.new }
+  let(:takeaway) { double :takeaway }
+
+  it "should have a name" do
+    customer.name = "Erica"
+    expect(customer.name).to eq("Erica")
+  end
+
 
 #   it "should be able to place an order" do
 #     customer.order_food(takeaway)
 #     expect(takeaway).to receive(:place_order)
 #     # expect(customer.order(takeaway)).to change{takeaway.orders_count}.by(1)
 #   end
-# end
+end
